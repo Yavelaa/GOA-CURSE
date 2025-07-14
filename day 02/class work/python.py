fdaf605bd106f4f1dc1a53986f4528a9718dc102
Yavelaa <tatoyavelashvili75@gmail.com>
2025-07-14 16:30:26 +0400

    commit

--- day 02/class work/python.py
+++ day 02/class work/python.py
@@ -1,1 +1,7 @@
-#no classwork
+num1 = 5
+num2 = 10.5
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 / num2)
+print(num1 * num2)
---
+++ day 04/homwork/python.py
@@ -0,0 +1,24 @@
+# Debugging-ი არის პაითონში კოდის შეცდომების აღმოჩენას და გამოსწორებას  რომ კოდმა იმუშაოს სწორად
+# კონკადინაცია პითონში ნიშნავს სტრინგების შეერთებას მაგალითად
+name = "tato"
+surname = "yavelashvili"
+
+print(name + surname)
+
+
+name = "nika"
+surname = "nioradze"
+print(name + surname) 
+
+name = "gio"
+surname = "cxadadze"
+print(name + surname)
+
+
+num = 10
+num2 = 20
+num3 = 30 
+num4 = 40
+num5 = 50
+print((num + num2 + num3 + num4 + num5) / 5)
+
