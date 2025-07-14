42e10295b53b172084a8fec4c844f8fba0e4b4d8
Yavelaa <tatoyavelashvili75@gmail.com>
2025-07-14 14:35:48 +0400

    commit

---
+++ day 02/home work/python.py
@@ -0,0 +1,24 @@
+#string ari raime monacemebis tipi romelic aucileblad unda chavsvat brchyalebshi 
+#magalitad chven shegvidzlia moviyvanot "tato", "st", "18" da es aucileblad unda iyos brchayalebshi chasmuli
+
+#integer aris is rac asaxavs mtel ricxvebs an raime monacemta tips  da nishnavs mtel ricxvs romelic brchyalebis gareshe shegvidzlia davwerot
+#rogoricaa 10 , 20 , 30  da ase shemdeg
+
+#float ari magalitad is cifrebi romelic ar aris mteli da nishnavs atwilads
+#magalitad 10.5 , 5.0 , 3,5
+
+number = 10
+word = "Car"
+print('number') # 10
+print(word)
+
+
+name = "tato"
+surname = "yavelashvili"
+
+age = 18
+print(name + " " + surname + " " + str(age))
+#imitoro strings intejers ver daumatebt 
+
+print(5+5) # gamoitans atis 
+print("5"+"5") # es aღiqmeba rogorc teqsti
