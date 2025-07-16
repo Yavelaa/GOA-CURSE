63c0c4eff3f0b2ca9e235a2fedec4eadaca27e1f
Yavelaa <tatoyavelashvili75@gmail.com>
2025-07-16 20:18:06 +0400

    commit

---
+++ day 06/home work/python.py
@@ -0,0 +1,12 @@
+#snake case -არის მაგალითად რამოდენიმე სიტყვის ერთობლიობა მაგალითად mynameis ეს პირდაპირ შესამჩნევად არიქნება ლამაზი ამიტომ გვაქვს snake case რომელსაც ავსახავთ ასე my_name_is
+
+
+num1 = input("enter first number: ")
+num2 = input("enter second number: ")
+
+print(num1+num2)  
+# იმის გამო რომ imputit რაც შემოგვყავს ყველაფერი stringia 
+
+num1 = int(input("enter first number: "))
+num2 = int(input("enter second number: "))
+print(num1+num2)
