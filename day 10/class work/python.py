a45a7c41bb99a3198f94a7dc315a8c04356b4a2f
Yavelaa <tatoyavelashvili75@gmail.com>
2025-07-26 13:56:51 +0400

    commit

---
+++ day 10/class work/python.py
@@ -0,0 +1,31 @@
+#  explycity - არის როდესაც შენი ხელით ცვლი რაიმეს კოდის წერის დროს 
+#  implicity - არის როდესაც კომპიუტერს ავტომატურად გამოაქვს პასუხი
+
+
+name = "tato"
+age = "18"
+
+bla = "My name is " + name + " . i m " + age + " years old"
+print (bla) # ho es bla imitoro sxva vermovifiqre dd :)
+
+number = int(input("enter your fav number: "))
+print(number ** 5)
+
+#if expression1 :
+        #statement_1
+         #statement_2
+         ....   
+   
+##   statement_3 
+     #statement_4
+     ....     
+   #elif expression3 : 
+    # statement_5 
+     #statement_6
+     ....................    
+   #else : 
+    # statement_7 
+     #statement_8
+
+
+
