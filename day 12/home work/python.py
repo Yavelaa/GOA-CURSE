8760d68341e083842538fc0adf7e41a56a69cd02
Yavelaa <tatoyavelashvili75@gmail.com>
2025-07-31 13:20:22 +0400

    commit

---
+++ day 12/home work/python.py
@@ -0,0 +1,34 @@
+#bolean - არის კოდი რომელსაც გამოაქვს ჩვენი კოდი არის true or false და ამის მიხედვით ვიგებთ თ რამდენად სწორია ჩვენი კოდი
+#binary code - არის რაიმე ტიპის ინფორმაცია რომელსაც კომპიუტერი გვაწვდის მაგრამ ჩვენ მას ვერდავინახავთ და ის გამოისახება ერთიანებით და ნულიანებით რაზეც მთლიანი კომპიუტერია აწყობილი
+
+print(5 == 5)
+print(3 != 4)
+print(5 > 3)
+print(2 < 6)
+print(10 >= 10) 
+print(5 <= 5)
+
+
+
+#მაგალითად თუ საიტზე შედიხარ და ასაკი მოგთხოვა
+
+age = 18
+
+age <= 18
+
+input("enetr your age: ")
+
+
+print(True and True) # true 
+print(True and False) # false
+print(False and True) # false
+print(True or True) # True
+print(True or False) # True
+
+
+
+number = int(input("enter your number: "))
+number % 2 == 0 or number % 5 == 0
+print(True) or print(False)
+
+
