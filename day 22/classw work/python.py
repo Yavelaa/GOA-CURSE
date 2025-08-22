48977d110ca4682ccd5ca409527381f38d1a554d
Yavelaa <tatoyavelashvili75@gmail.com>
2025-08-22 21:21:53 +0400

    commit

---
+++ day 22/classw work/python.py
@@ -0,0 +1,17 @@
+i=1
+while i <= 10:
+    print(i)
+    i += 1
+print("end of while loop")
+
+i = 0
+while i <= 100:
+    print(i)
+    i+=2
+print("end of while loop")
+
+i = 10
+while i >= 0:
+    print(i)
+    i -= 1
+print("end of while loop")
